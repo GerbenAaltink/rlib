a20988a4f5e56d4501614454cc3d6bd318ca816b
Gerben Aaltink <gerbenaaltink@gmail.com>
2024-10-06 08:06:50 +0200

    New release

--- rtemp.h
+++ rtemp.h
@@ -1,14 +1,24 @@
 #include "rtypes.h"
-
+#include <pthread.h>
 #ifndef RTEMPC_SLOT_COUNT
 #define RTEMPC_SLOT_COUNT 20
 #endif
 #ifndef RTEMPC_SLOT_SIZE
-#define RTEMPC_SLOT_SIZE 1024 * 64
+#define RTEMPC_SLOT_SIZE 1024 * 64 * 128
 #endif
+
+bool _rtempc_initialized = 0;
+pthread_mutex_t _rtempc_thread_lock;
+bool rtempc_use_mutex = true;
 byte _current_rtempc_slot = 0;
 char _rtempc_buffer[RTEMPC_SLOT_COUNT][RTEMPC_SLOT_SIZE];
 char *rtempc(char *data) {
+
+    if (!_rtempc_initialized && rtempc_use_mutex) {
+        _rtempc_initialized = true;
+        pthread_mutex_init(&_rtempc_thread_lock, NULL);
+    }
+    pthread_mutex_lock(&_rtempc_thread_lock);
 
     uint current_rtempc_slot = _current_rtempc_slot;
     _rtempc_buffer[current_rtempc_slot][0] = 0;
@@ -17,6 +27,8 @@ char *rtempc(char *data) {
     if (_current_rtempc_slot == RTEMPC_SLOT_COUNT) {
         _current_rtempc_slot = 0;
     }
+    if (rtempc_use_mutex)
+        pthread_mutex_unlock(&_rtempc_thread_lock);
     return _rtempc_buffer[current_rtempc_slot];
 }
 
